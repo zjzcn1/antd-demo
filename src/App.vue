fa0ebd61df04e3dad9d111f3209d54ebf5c7c82c
zjzcn1 <43628956+zjzcn1@users.noreply.github.com>
2021-06-23 16:02:23 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-config-provider>
+    <router-view></router-view>
+  </a-config-provider>
+</template>
+
+<script>
+  import { defineComponent} from "vue";
+
+  export default defineComponent({
+    name: 'App',
+    setup() {
+      return {
+      }
+    }
+  })
+</script>
