8dbea3020b8bc823d9501faaa132eacc7197e9a0
zjzcn1 <43628956+zjzcn1@users.noreply.github.com>
2021-06-23 16:03:00 +0800

    Create Main.vue

---
+++ src/pages/Main.vue
@@ -0,0 +1,105 @@
+<template>
+  <a-layout>
+    <a-layout-sider v-model:collapsed="collapsed" width="240" :trigger="null" collapsible>
+      <a-layout-header style="background-color: #002140; padding-left: 22px">
+          <img style="height: 30px; width: 30px ; vertical-align: middle;" src="../assets/logo.png"/>
+          <span style="padding-left: 24px; color: #fff; font-size: 18px; font-weight: 400;">{{collapsed?'':'测试系统'}}</span>
+      </a-layout-header>
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys" style="overflow: hidden auto; height: calc(100vh - 64px)">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+                v-if="collapsed"
+                class="trigger"
+                @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)"/>
+
+        <a-breadcrumb style="display: inline-block">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+          <a-breadcrumb-item><a href="">Application List</a></a-breadcrumb-item>
+          <a-breadcrumb-item>An Application</a-breadcrumb-item>
+        </a-breadcrumb>
+      </a-layout-header>
+      <a-layout-content style="height: calc(100vh - 120px); overflow: hidden auto;">
+        <div style="background: #fff; margin: 24px; height: 500px">hehe</div>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+  import {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+  } from '@ant-design/icons-vue';
+  import { defineComponent, ref } from 'vue';
+
+  export default defineComponent({
+    components: {
+      UserOutlined,
+      VideoCameraOutlined,
+      UploadOutlined,
+      MenuUnfoldOutlined,
+      MenuFoldOutlined,
+    },
+
+    setup() {
+      return {
+        selectedKeys: ref(['1']),
+        collapsed: ref(false),
+      };
+    },
+  });
+</script>
+<style>
+  .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
+
+  .trigger:hover {
+    color: #1890ff;
+  }
+
+</style>
