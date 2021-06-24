6237d741bbcc1d763ac34975fb1595b077e2b525
zjzcn1 <43628956+zjzcn1@users.noreply.github.com>
2021-06-24 16:19:50 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,70 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-
-
-import Main from '@/pages/Main.vue';
-
 const routes = [
   {
-    title: 'empty',
     path: '/',
-    component: Main,
-  // },
-  // {
-  //   title: 'app.global.menu.notfound',
-  //   path: '/:pathMatch(.*)*',
-  //   component: () => import('@/views/404/index.vue'),
-  }
-]
-
-const router = createRouter({
-  scrollBehavior(/* to, from, savedPosition */) {
-    return { top: 0 }
+    component: () => import("@/pages/Layout"),
+    meta: {hidden: false, leaf: true},
+    children: [
+      {
+        path: '/home',
+        name: '11',
+        meta: {title: '测试1', icon: 'MenuFoldOutlined'},
+        component: () => import("@/pages/403")
+      }
+    ]
   },
-  history: createWebHashHistory(process.env.BASE_URL),
-  routes: routes,
-});
+  {
+    path: '/',
+    name: '01',
+    component: () => import("@/pages/Layout"),
+    meta: {hidden: false, leaf: false, title: '测试1', icon: 'MenuFoldOutlined'},
+    children: [
+      {
+        path: '/home1',
+        name: '112',
+        meta: {title: '测试1', icon: 'MenuFoldOutlined'},
+        component: () => import("@/pages/404")
+      },
+      {
+        path: '/home2',
+        name: '113',
+        meta: {title: '测试2', icon: 'MenuFoldOutlined'},
+        component: () => import("@/pages/403")
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: () => import("@/pages/Layout"),
+    name: '',
+    meta: {hidden: true},
+    children: [
+      {
+        path: '/404',
+        name: '页面未找到',
+        meta: {},
+        component: () => import("@/pages/404")
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: () => import("@/pages/Layout"),
+    name: '',
+    meta: {hidden: true},
+    children: [
+      {
+        path: '/403',
+        name: '没有权限',
+        meta: {},
+        component: () => import("@/pages/403")
+      }
+    ]
+  },
+  // {
+  //   path: '/:catchAll(.*)',
+  //   redirect: {path: '/404'},
+  //   meta: {hidden: true},
+  // }
+];
 
-export default router;
+export default routes;
