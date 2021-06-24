91a2b217dbe2a1a2fdc881634ca29e27c8b7609e
zjzcn1 <43628956+zjzcn1@users.noreply.github.com>
2021-06-24 16:19:25 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 import { createApp } from 'vue';
 import Antd from 'ant-design-vue';
-import App from './App';
 import 'ant-design-vue/dist/antd.css';
-import router from '@/routes';
+import * as antIcons from '@ant-design/icons-vue'
+import App from './App';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from '@/routes';
 
 const app = createApp(App);
 app.config.productionTip = false;
 
+// antd
 app.use(Antd);
+Object.keys(antIcons).forEach(key => {
+  app.component(key, antIcons[key])
+});
+app.config.globalProperties.$antIcons = antIcons;
+
+// router
+const router = createRouter({
+  scrollBehavior(/* to, from, savedPosition */) {
+    return { top: 0 }
+  },
+  history: createWebHashHistory(),
+  routes: routes,
+});
 app.use(router);
+
+// mount
 app.mount('#app');
